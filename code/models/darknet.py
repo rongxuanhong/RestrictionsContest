32ac8ea046dc7ad75e9a5aa7d56ccd82b4d6783d
rongxuanhong <389817374@qq.com>
2019-03-04 14:46:21 +0800

    darknet-19

---
+++ code/models/darknet.py
@@ -0,0 +1,115 @@
+import torch.nn as nn
+import torchvision.models as m
+import math
+
+
+class DarkNet(nn.Module):
+    def __init__(self, module_list, num_classes, init_weights=True):
+        super(DarkNet, self).__init__()
+        self.module_list = module_list
+        self.classifier = nn.Sequential(
+            nn.Conv2d(1024, num_classes, 1, 1, 0),
+            nn.BatchNorm2d(num_classes),
+            nn.ReLU(True),
+            nn.AvgPool2d(7, 1),
+
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        for module in self.module_list:
+            x = module(x)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+def _conv2d_bn_relu(in_channels, filters, kernel_size, stride, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, filters, kernel_size, stride, padding, bias=False),
+        nn.BatchNorm2d(filters),
+        nn.ReLU(True),
+        nn.Dropout(),
+    )
+
+
+def _maxpool():
+    return nn.Sequential(
+        nn.MaxPool2d(2, 2)
+    )
+
+
+def _make_module_list():
+    modules = nn.ModuleList()
+    modules.append(_conv2d_bn_relu(3, 32, 3, 1, 1))
+    modules.append(_maxpool())
+
+    modules.append(_conv2d_bn_relu(32, 64, 3, 1, 1))
+    modules.append(_maxpool())
+
+    modules.append(_conv2d_bn_relu(64, 128, 3, 1, 1))
+    modules.append(_conv2d_bn_relu(128, 64, 1, 1, 0))
+    modules.append(_conv2d_bn_relu(64, 128, 3, 1, 1))
+    modules.append(_maxpool())
+
+    modules.append(_conv2d_bn_relu(128, 256, 3, 1, 1))
+    modules.append(_conv2d_bn_relu(256, 128, 3, 1, 0))
+    modules.append(_conv2d_bn_relu(128, 256, 3, 1, 1))
+    modules.append(_maxpool())
+
+    modules.append(_conv2d_bn_relu(256, 512, 3, 1, 1))
+    modules.append(_conv2d_bn_relu(512, 256, 3, 1, 0))
+    modules.append(_conv2d_bn_relu(256, 512, 3, 1, 1))
+    modules.append(_conv2d_bn_relu(512, 256, 3, 1, 0))
+    modules.append(_conv2d_bn_relu(256, 512, 3, 1, 1))
+    modules.append(_maxpool())
+
+    modules.append(_conv2d_bn_relu(512, 1024, 3, 1, 1))
+    modules.append(_conv2d_bn_relu(1024, 512, 3, 1, 0))
+    modules.append(_conv2d_bn_relu(512, 1024, 3, 1, 1))
+    modules.append(_conv2d_bn_relu(1024, 512, 3, 1, 0))
+    modules.append(_conv2d_bn_relu(512, 1024, 3, 1, 1))
+    modules.append(_maxpool())
+
+    return modules
+
+
+def darknet(num_classes, init_weights=True):
+    return DarkNet(_make_module_list(), num_classes=num_classes, init_weights=init_weights)
+
+
+def get_number_of_param(model):
+    """get the number of param for every element"""
+    count = 0
+    for param in model.parameters():
+        param_size = param.size()
+        count_of_one_param = 1
+        for dis in param_size:
+            count_of_one_param *= dis
+        print(param.size(), count_of_one_param)
+        count += count_of_one_param
+        print(count)
+    print('total number of the model is %d' % count)
+
+
+def test_net():
+    model = darknet(5)
+    get_number_of_param(model)
+
+
+if __name__ == '__main__':
+    test_net()
