6a1d14d275ca5a1597144f172c992ea28feb5d99
rongxuanhong <389817374@qq.com>
2019-03-06 14:34:41 +0800

    darknet-19

--- code/models/darknet.py
+++ code/models/darknet.py
@@ -2,8 +2,7 @@ import torch.nn as nn
 import torchvision.models as m
 import math
 from utils.utils import *
-
-m.resnet18()
+from utils.switch_norm_torch import SwitchNorm2d
 
 
 class DarkNet(nn.Module):
@@ -11,7 +10,7 @@ class DarkNet(nn.Module):
         super(DarkNet, self).__init__()
         self.module_list = module_list
         self.classifier = nn.Sequential(
-            nn.Conv2d(1024, num_classes, 1, 1, 0),
+            nn.Conv2d(1024, num_classes, 1, 1, bias=False),
             nn.BatchNorm2d(num_classes),
             nn.ReLU(True),
             nn.AvgPool2d(7, 1),
@@ -21,31 +20,33 @@ class DarkNet(nn.Module):
             self._initialize_weights()
 
     def forward(self, x):
+        print(x.shape)
         for module in self.module_list:
             x = module(x)
+            print(x.shape)
         print(x.shape)
         x = self.classifier(x)
-        return x.squeeze()
+        x = x.squeeze()
+        print(x.shape)
+        return x
 
     def _initialize_weights(self):
+        # Official init from torch repo.
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-                if m.bias is not None:
-                    m.bias.data.zero_()
+                nn.init.kaiming_normal_(m.weight.data)
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
-                m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
 
 
-def _conv2d_bn_relu(in_channels, filters, kernel_size, stride, padding):
+def _conv2d_bn_relu(in_channels, out_channels, kernel_size, stride, padding):
     return nn.Sequential(
-        nn.Conv2d(in_channels, filters, kernel_size, stride, padding, bias=False),
-        nn.BatchNorm2d(filters),
+        nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+        # SwitchNorm2d(out_channels),
+        nn.BatchNorm2d(out_channels),
         nn.LeakyReLU(0.1, inplace=True),
         # nn.Dropout(),
     )
@@ -76,16 +77,16 @@ def _make_module_list():
     modules.append(_maxpool())
 
     modules.append(_conv2d_bn_relu(256, 512, 3, 1, 1))
-    modules.append(_conv2d_bn_relu(512, 256, 3, 1, 0))
+    modules.append(_conv2d_bn_relu(512, 256, 3, 1, 1))
     modules.append(_conv2d_bn_relu(256, 512, 3, 1, 1))
-    modules.append(_conv2d_bn_relu(512, 256, 3, 1, 0))
+    modules.append(_conv2d_bn_relu(512, 256, 3, 1, 1))
     modules.append(_conv2d_bn_relu(256, 512, 3, 1, 1))
     modules.append(_maxpool())
 
     modules.append(_conv2d_bn_relu(512, 1024, 3, 1, 1))
-    modules.append(_conv2d_bn_relu(1024, 512, 3, 1, 0))
+    modules.append(_conv2d_bn_relu(1024, 512, 3, 1, 1))
     modules.append(_conv2d_bn_relu(512, 1024, 3, 1, 1))
-    modules.append(_conv2d_bn_relu(1024, 512, 3, 1, 0))
+    modules.append(_conv2d_bn_relu(1024, 512, 3, 1, 1))
     modules.append(_conv2d_bn_relu(512, 1024, 3, 1, 1))
 
     return modules
@@ -97,10 +98,13 @@ def darknet(num_classes, init_weights=True):
 
 def test_net():
     import torch
-    x = torch.randn((1, 3, 224, 224))
+    x = torch.randn((2, 3, 224, 224))
     model = darknet(5)
+    model(x)
     get_number_of_param(model)
 
 
 if __name__ == '__main__':
     test_net()
+    # model = m.resnet101(pretrained=False)
+    # get_number_of_param(model)
